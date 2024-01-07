2ac7a6a292c9dee46961b8dccd7939aa9ee808d2
innes <innes@innesui-MacBookAir.local>
2024-01-07 14:56:39 +0900

    id, title, overview, poster path 불러오기

--- js/js.js
+++ js/js.js
@@ -9,5 +9,15 @@ const options = {
 
   fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => {
+        // console.log(response)
+        // console.log(response['results'][0]['id'])
+        let card_ID = response['results'][0]['id'];
+        let title = response['results'][0]['original_title'];
+        let overview = response['results'][0]['overview'];
+        let img_link = response['results'][0]['poster_path'];
+
+        // console.log(card_ID, title, overview, img_link);
+    })
     .catch(err => console.error(err));
+    
